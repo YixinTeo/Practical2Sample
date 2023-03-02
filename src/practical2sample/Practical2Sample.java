7736033deec0dcdddb6d18fbbfbf44534f7f2e46
YixinTeo <91593436+YixinTeo@users.noreply.github.com>
2023-03-02 10:34:38 +0800

    myUpdate

--- src/practical2sample/Practical2Sample.java
+++ src/practical2sample/Practical2Sample.java
@@ -15,6 +15,7 @@ public class Practical2Sample {
      */
     public static void main(String[] args) {
         System.out.println("Hi Hello");
+        System.out.println("Welcome");
     }
     
 }
